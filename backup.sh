b59056ecd411bc90c6f8f536e78361cab5ccd501
D0n9X1n <D0n9X1n@outlook.com>
2023-09-03 22:34:22 +0800

    update

--- backup.sh
+++ backup.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 #
 
-Script=$1
-UserName=$2
-Password=$3
-Count=$4
+UserName=$1
+Password=$2
+Count=$3
 GarminFolder=~/Garmin/$UserName
 
 if [ -d $GarminFolder ]; then
     rm -R $GarminFolder
 fi
 
+if [ -n $Count ]; then
+    Count=20
+fi
+
+echo "using script:$Script to backup garmin activities with count:$Count"
 python3 $Script -c $Count -d $GarminFolder --format tcx -u --username $UserName --password $Password
--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
-echo "copy libs & backup.sh to /usr/local/bin"
+
+echo "copy backup.sh to /usr/local/bin"
 chmod +x ./backup.sh
 sudo cp ./backup.sh /usr/local/bin/
-sudo cp ./gcexport.py /usr/local/bin/
+
+echo "export Script=$(pwd)/gcexport.py" >> ~/.oh-my-zsh/custom/garmin.zsh

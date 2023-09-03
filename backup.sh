55652e42068a41abfa39377776724aa95f115a1e
D0n9X1n <D0n9X1n@outlook.com>
2023-09-03 22:38:57 +0800

    update count param

--- backup.sh
+++ backup.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 #
 
+if [ ! -n "$3" ]; then
+    Count=20
+else
+    Count=$3
+fi
+
 UserName=$1
 Password=$2
-Count=$3
 GarminFolder=~/Garmin/$UserName
 
 if [ -d $GarminFolder ]; then
     rm -R $GarminFolder
 fi
 
-if [ -n $Count ]; then
-    Count=20
-fi
-
 echo "using script:$Script to backup garmin activities with count:$Count"
-python3 $Script -c $Count -d $GarminFolder --format tcx -u --username $UserName --password $Password
+# python3 $Script -c $Count -d $GarminFolder --format tcx -u --username $UserName --password $Password

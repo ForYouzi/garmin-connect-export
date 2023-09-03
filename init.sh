d523a70d079470b5011833d663a33d52358e647e
D0n9X1n <D0n9X1n@outlook.com>
2023-09-03 22:46:34 +0800

    update ini shell

--- init.sh
+++ init.sh
@@ -5,4 +5,8 @@ echo "copy backup.sh to /usr/local/bin"
 chmod +x ./backup.sh
 sudo cp ./backup.sh /usr/local/bin/
 
+if [ -d ~/.oh-my-zsh/custom/garmin.zsh ]; then
+    rm ~/.oh-my-zsh/custom/garmin.zsh
+fi
+
 echo "export Script=$(pwd)/gcexport.py" >> ~/.oh-my-zsh/custom/garmin.zsh

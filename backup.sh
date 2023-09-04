21dc6525b9d7c6d1d651be33cbbab9aa3d1c14a3
D0n9X1n <D0n9X1n@outlook.com>
2023-09-04 23:29:39 +0800

    update

--- backup.sh
+++ backup.sh
@@ -16,4 +16,18 @@ if [ -d $GarminFolder ]; then
 fi
 
 echo "using script:$Script to backup garmin activities with count:$Count"
-python3 $Script -c $Count -d $GarminFolder --format tcx -u --username $UserName --password $Password
+echo "=============================================================================="
+echo "!!! Press Y to continue or others to exit. !!!"
+echo "=============================================================================="
+read cmd
+if [ $cmd == 'Y' ] || [ $cmd == 'y' ]; then
+    python3 $Script -c $Count -d $GarminFolder --format tcx -u --username $UserName --password $Password
+    echo "=============================================================================="
+    echo "!!! Done Export to $GarminFolder !!!"
+    echo "=============================================================================="
+else
+    echo "=============================================================================="
+    echo "!!! Exit !!!"
+    echo "=============================================================================="
+fi
+
